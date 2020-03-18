a6fb376f21523126167034a3390a209a75b09ec1
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-18 15:13:56 -0500

    [+] App > setState: Get lyrics from API song

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Form from './components/Form';
 
 function App() {
 
   /** Hook: Define State */
   const 
-    [ searchSongLyrics, setSearchSongLyrics ] = useState({});
+    [ searchSongLyrics, setSearchSongLyrics ] = useState( {} ),
+    [ lyrics, setLyrics ] = useState( '' );
+
+  /** Hook: Traking State 'searchTerms' */
+  useEffect( () => {
+
+    const getDataApiLyrics = async () => {
+
+        /** Validate */
+        if( Object .keys( searchSongLyrics ) .length === 0 ) {
+          return;
+        }
+
+        const 
+          { artist, track } = searchSongLyrics,                       // Destructuring State
+
+          url = `https://api.lyrics.ovh/v1/${ artist }/${ track }`,
+          resolve = await fetch( url ),
+          data = await resolve .json();
+
+        console .log( 'lyricsovh', data );
+        setLyrics( data .lyrics );      // Update State 'lyrics'
+        
+    }
+    getDataApiLyrics();
+
+  }, [ searchSongLyrics ] );
 
   return (
     <Fragment>
