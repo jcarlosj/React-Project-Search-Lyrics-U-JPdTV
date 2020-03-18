3c688fcf82d92da319f5463a1e2ef476dae86df0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-03-18 12:42:57 -0500

    [+] Component > Form: Form fields

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Form from './components/Form';
 function App() {
   return (
     <Fragment>
-      <h1>Búsqueda de letras de canciones.</h1>
       <Form />
     </Fragment>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,36 @@ import React from 'react';
 
 const Form = () => {
     return (
-        <h2>Formulario</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        
+            <a className="navbar-brand" href="#">Letras de canciones</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarTogglerDemo02">
+                
+                <form className="form-inline my-2 my-lg-0">
+                    <input
+                        type="text" 
+                        name="artist"
+                        placeholder="Nombre Artísta" 
+                        className="form-control mr-sm-2" 
+                    />
+                    <input 
+                        type="text" 
+                        name="track"
+                        placeholder="Nombre de la canción" 
+                        className="form-control mr-sm-2" 
+                    />
+                    <button 
+                        type="button"
+                        className="btn btn-outline-primary my-2 my-sm-0" 
+                    >Buscar</button>
+                </form>
+            </div>
+        
+        </nav>
     );
 }
 
